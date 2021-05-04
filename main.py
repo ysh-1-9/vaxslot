e9ff027a2e22884dbce60f4a917b9c0f25ab7169
ysh-1-9 <51337798+ysh-1-9@users.noreply.github.com>
2021-05-05 01:47:53 +0530

    added function prototypes and comments

--- main.py
+++ main.py
@@ -43,7 +43,43 @@ def function(state, district, age, weeks):
             print('Slot Available')
     if len(slots)==0:
         print('No slots Available')
+        
+        
+        
+# [{'emailID':'adityaranjha786@gmail.com','state':'west bengal','district':'kolkata', 'age': 18},{....},{....}]
 
+# {'andhra pradesh':{'kalimpondi': [usr1,usr2...], 'machu pichu': [usr3, usr4...]} }
+
+def updateDB():
+    # get states and districts possibly locally
+    # look through the current database of (districtID,age) -> [list of centers with availability within next 12 weeks]
+    # for every state,district,age, query the cowin thing and update to a local variable
+    # go through the new db. if the session was there before and numbers same, dont do anything. if the numbers changes, save it to a changedDB. if it wasnt there before, save it to a newlyaddedDB.
+    # finally update the on disk db to the new db.
+    pass
+
+def notify():
+    # go through the users in the (districtID,age)->list of users for every districtID and age
+    #  for each districtID,age come up with 3 things - a> email in general likhna hai?, b> updates kya kya dene hai and c> newadditions kya kya batane hai
+    # then for each districtID send (or don't) the respective email to all users from that category
+    pass
+
+
+def createEmail(updates,additions):
+    # returns a parsed sexy ass email object ya whatever
+    pass
+
+def addUser(emailID, state, district, age):
+    # create an email about ALL available stuff as additions, w some welcome bs
+    # send email
+    # save user to functional database
+    # save user to permanent database
+    pass
+
+def deleteUser(emailID):
+    # remove user from functional database
+    # send bullshit liberal email
+    pass
 
 
 function('west bengal','kolkata', 45, 8)
